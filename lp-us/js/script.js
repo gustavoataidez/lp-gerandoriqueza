c30381e0e47a8125509b09675081bfb15758c856
gustavoataidez <luisgustavoataidee@gmail.com>
2023-04-07 13:11:10 -0300

    Organizando o arquivo script.js e as pastas

--- lp-us/js/script.js
+++ lp-us/js/script.js
@@ -1,1 +1,18 @@
-$('#botao').hide().delay(3000).fadeIn('slow')
+function detectarDispositivo() {
+    const dispositivoMovel = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    if (dispositivoMovel) {
+        // Se for um dispositivo móvel, redireciona para a página mobile.html
+        window.location.href = "indexm.html";
+    }
+}
+
+window.onload = function () {
+    var links = document.getElementsByTagName("a");
+    for (var i = 0, n = links.length; i < n; i++) {
+        var href = links[i].href.trim() +
+                (links[i].href.indexOf("?") > 0 ? '&' : '?') +
+                document.location.search.replace('?', '').toString();
+        links[i].href = href;
+    }
+}
+
